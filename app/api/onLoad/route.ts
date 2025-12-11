2a966fd17311d5182a684ca85e84794442148d82
Eddie Yeung <eycs0317@gmail.com>
2025-12-11 13:33:58 -0800

    remove log

--- app/api/onLoad/route.ts
+++ app/api/onLoad/route.ts
@@ -9,11 +9,12 @@ export async function POST(request: Request) {
   const uid = body.uid
 
   const rawData = getData(uid);
-  console.log('raw data received in API:', rawData);
+
   const baseResult = createBase({data: rawData.awards, count: rawData.count});
-  console.log('base data received in API:', baseResult);
+
   const displayResult = createDisplay(rawData.count);
-  console.log('  displayResult received in API:', displayResult);
+
   const hasItemNameMode  = rawData.awards[0]?.item ? true : false;
+
   return NextResponse.json({baseResult, displayResult, hasItemNameMode});
 }
